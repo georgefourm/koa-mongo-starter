787f35c8bc898d2dc0688925aa9700b11dcc318f
George Fourmouzis <foogee@monospacelabs.com>
2018-05-31 12:00:20 +0300

    Refactor JWT helper

--- src/middleware/jwt.js
+++ src/middleware/jwt.js
@@ -1,28 +1,34 @@
 const config = require('config')
 const jwt = require('jsonwebtoken')
 
+const AUTH_HEADER = 'Authorization'
+const TOKEN_PREFIX = 'Bearer '
+
+const parseAuthHeader = ctx => {
+    const header = ctx.get(AUTH_HEADER)
+    if (!header) {
+        ctx.throw(400,"Missing Authorization header")
+    }
+    
+    if (!header.startsWith(TOKEN_PREFIX)) {
+        ctx.throw(400,"Malformed Authorization header")
+    }
+    
+    return header.substr(TOKEN_PREFIX.length)
+}
+
 module.exports = async (ctx,next) => {
     ctx.jwt = (token = null) => {
         
         if (token == null) {
-            const header = ctx.get('Authorization')
-            if (!header) {
-                ctx.throw(400,"Token not provided")
-            }
-            
-            const headerParts = header.split(" ")
-            if (headerParts.length != 2) {
-                ctx.throw(400,"Malformed Authorization header")
-            }
-            
-            token = headerParts[1]
+            token = parseAuthHeader(ctx)
         }
 
         const jwtSecret = config.get('jwt.secret')
         
         return new Promise((resolve,reject)=>{
             jwt.verify(token, jwtSecret,
-                function(err, decoded) {
+                (err, decoded) => {
                     if(err) return reject(err)
                     return resolve(decoded)
                 }
