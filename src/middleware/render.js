e95813f06f065769aca6a6cd3b7705b826e492fa
George Fourmouzis <foogee@monospacelabs.com>
2018-05-31 12:00:04 +0300

    Set pug options to be optional

--- src/middleware/render.js
+++ src/middleware/render.js
@@ -1,7 +1,7 @@
 const pug = require('pug')
 
 module.exports = async (ctx, next) => {
-    ctx.render = (file,options) => {
+    ctx.render = (file,options = {}) => {
         ctx.type = 'text/html'
         ctx.body = pug.renderFile(__dirname+`/../front/templates/${file}.pug`, options)
     }
